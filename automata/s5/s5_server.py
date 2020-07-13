87e65a1d724a064c5b6b77e08578e6a3ae18d237
liuliang22333 <qiyuyoulan@gmail.com>
2020-07-13 15:03:13 +0800

    no message

--- automata/s5/s5_server.py
+++ automata/s5/s5_server.py
@@ -4,7 +4,7 @@ from automata.s5.s5_server_atmt import *
 class s5_server(object):
     def __init__(self, dmac, smac, sref, iface):
         self.s5_server_atmt = None
-        self.is_stopped = True
+        self.is_stopped = False
         self.cotp_params = S5_COTP_Params(dmac, smac, sref, iface)
         self._sever_cotp_socket = COTPSocket(dmac=dmac, smac=smac, sref=sref, iface=iface)
 
